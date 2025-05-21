557a5c6f55cafabc53c65f757a88f2a0e58fd644
ecschul1 <schulte777@yahoo.com>
2025-05-20 20:21:30 -0700

    base settings file

---
+++ settings.py
@@ -0,0 +1,27 @@
+sleep_constant = 1.3
+iguanadon = "GACHAIGUANADON"
+drop_off = "GACHADEDI"
+bed_spawn = "GACHARENDER"
+berry_station = "GACHABERRYSTATION"
+grindables = "GACHAGRINDABLES"
+berry_type = "mejoberry"
+station_yaw = -123.29
+render_pushout = 165.96
+external_berry = False # True or False
+height_ele = 2
+height_grind = 3
+command_prefix = "%"
+singleplayer = False
+server_number = 9013
+crafting = False
+seeds_230 = False
+
+# you need to put in you channel IDs and your bot API
+log_channel_gacha = 1
+log_active_queue = 1
+log_wait_queue = 1
+discord_api_key = "" 
+
+
+if __name__ =="__main__":
+    pass
